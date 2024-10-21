3c6479e505be433089b7213196e16d641a9fbe2c
Vivek Gawar <vivekgaver2016@gmail.com>
2024-10-22 00:50:38 +0530

    CRUD Buttons work now

--- app.js
+++ app.js
@@ -48,6 +48,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const userModel = require('./models/user')
 
 app.set("view engine", "ejs")
 app.use(express.json())
@@ -58,13 +59,27 @@ app.get('/', (req,res) => {
     res.render('index')
 })
 
-app.post('/create', (req,res) => {
-
+app.post('/create', async (req,res) => {
+    let {name, email, image} = req.body
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    })
+    res.send(createdUser);
 })
 
 
-app.get('/read', (req,res) => {
-    res.render('read')
+app.get('/read', async (req,res) => {
+    let users = await userModel.find();
+    console.log(users)
+    res.render("read", { users });
+})
+
+app.get('/delete/:id', async (req,res) => {
+    let users = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect('/read')   
 })
 
 app.listen(3000)
+
